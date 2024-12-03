a3843036d35b7b9183e7dc175c9912dc4c0fca3b
kde-la-c <kdelacolinaplant@gmail.com>
2024-12-03 19:15:13 +0100

    finished second ex day3

--- Day03/main.cpp
+++ Day03/main.cpp
@@ -11,17 +11,21 @@ int	numlen(int nb)
 
 int	calc_line(string line)
 {
+
 	int	pos = 0, i, j, nb1, nb2, ret = 0, size = line.size();
 	while (pos < size && i != string::npos)
 	{
-		// this_thread::sleep_for (std::chrono::seconds(1));
+		// this_thread::sleep_for (std::chrono::milliseconds(100));
 		i = line.find("mul(", pos);
 		j = line.find("don't()", pos);
-		// cout << "i " << i << ", j " << j << endl;
 		if (j < i && j != string::npos)
 		{
+		// cout << "i " << i << ", j " << j << endl;
 			pos = j + 7;
 			j = line.find("do()", pos);
+			// cout << "pos " << pos << ", j " << j << endl;
+			if (j == string::npos)
+				break;
 			pos = j + 4;
 		}
 		else
@@ -37,33 +41,36 @@ int	calc_line(string line)
 					pos += numlen(nb2);
 					if (line.at(pos) == ')')
 					{
-						cout << nb1 << ',' << nb2 << endl;
+						// cout << nb1 << ',' << nb2 << endl;
 						ret += nb1 * nb2;
 					}
 				}
 			}
 		}
 	}
+	// this_thread::sleep_for (std::chrono::seconds(1));
 	return (ret);
 }
 
 int main()
 {
 	ifstream		input("input.txt");
-	string			line;
-	list<string>	lines;
+	string			line, lines;
+	// list<string>	lines;
 	int				ret = 0;
 
 	for (int i = 0; getline(input, line); i++)
 	{
-		lines.push_back(line);
+		// lines.push_back(line);
+		lines.append(line);
 	}
 
-	list<string>::iterator	it = lines.begin();
-	for (; it != lines.end(); it++)
-	{
-		ret += calc_line(*it);
-	}
+	// list<string>::iterator	it = lines.begin();
+	// for (; it != lines.end(); it++)
+	// {
+	// 	ret += calc_line(*it);
+	// }
+	ret = calc_line(lines);
 	cout << ret << endl;
 	return (0);
 }

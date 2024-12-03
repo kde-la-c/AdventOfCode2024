3528b5b737ed2e4ac48e8d10d91ad566dd18ae5d
kde-la-c <kdelacolinaplant@gmail.com>
2024-12-03 01:28:36 +0100

    day2 done

--- Day02/main.c
+++ Day02/main.c
@@ -1,12 +1,32 @@
 #include "../libft.h"
 
+int	*remove_one(int report[10], int pos)
+{
+	// printf("input %i %i %i %i %i\n", report[0], report[1], report[2], report[3], report[4]);
+	int *ret = malloc(sizeof(int) * 10);
+	for (int i = 0; i < 10; i++)
+	{
+		ret[i] = report[i];
+	}
+	while (ret[pos + 1])
+	{
+		if (pos >= 0)
+			ret[pos] = ret[pos + 1];
+		pos++;
+	}
+	while (pos < 9)
+		ret[pos++] = 0;
+	return (ret);
+}
+
 int main()
 {
-	int		report[1000][10], fd, ret = 0, lines = 4;
+	int		lines = 1000;
+	int		report[lines][10], *rep, fd, ret = 0;
 	char	**split, *line;
 
-	fd = open("test.txt", O_RDONLY);
-	ft_bzero(&report, sizeof(int) * 10000);
+	fd = open("input.txt", O_RDONLY);
+	ft_bzero(&report, sizeof(int) * (lines * 10));
 	for (int i = 0; i < lines; i++)
 	{
 		line = get_next_line(fd);
@@ -19,32 +39,42 @@ int main()
 		ft_dfree((void **)split);
 		report[i][9] = ft_mathei((void **)split);
 	}
+
 	for (int i = 0; i < lines; i++)
 	{
-		int	balance = 0, diff = -1, feq = 0;
-		for (int j = 0; report[i][j] && report[i][j + 1]; j++)
+		rep = report[i];
+		int safe = 0;
+		for (int k = 0; k < report[i][9] + 1; k++)
 		{
-			int	nb = report[i][j], next = report[i][j + 1];
-			if (nb < next || nb == next && balance > 0)
-				balance--;
-			else if (nb > next || nb == next && balance < 0)
-				balance++;
-			else
-				feq = 1;
-			if (abs(nb - next) > 3)
-				if (diff == -1)
-					diff = j;
-				else
-					diff = -2;
+			// printf("%i %i %i %i %i\n", rep[0], rep[1], rep[2], rep[3], rep[4]);
+			int	balance = 0, diff = 0, feq = 0/* , safe = 0 */;
+			for (int j = 0; rep[j] && rep[j + 1]; j++)
+			{
+				int	nb = rep[j], next = rep[j + 1];
+				if (nb < next || nb == next && balance > 0)
+					balance--;
+				else if (nb > next || nb == next && balance < 0)
+					balance++;
+				// else
+				// 	feq = 1;
+				if (abs(nb - next) > 3)
+				{
+					diff = 1;
+					// if (diff == -1)
+					// 	diff = j;
+					// else
+					// 	diff = -2;
+				}
+			}
+			if ((!diff && abs(balance) == report[i][9] - 1 && !k) || (!diff && abs(balance) == report[i][9] - 2 && k))
+			{
+				safe = 1;
+				break;
+			}
+			rep = remove_one(report[i], k);
 		}
-		int	bol = (diff == -1 && abs(balance) == report[i][9] - 1)
-			|| (diff == -1 && ((abs(balance) == report[i][9] - 2 && !feq) || (abs(balance) == report[i][9] - 2) && feq))
-			|| (diff > -1 && abs(balance) == report[i][9] - 1);
-		printf("%i : bal %i, diff %i, feq %i, sz %i\n", bol, balance, diff, feq, report[i][9]);
-		if (bol)
-			ret++;
 		// if (!diff && abs(balance) == report[i][9] - 1)
-		// 	ret++;
+			ret += safe;
 	}
 	printf("%i\n", ret);
 }

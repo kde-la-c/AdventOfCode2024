05f3cf0252d3b77612afb3d42f428b1bbf13196f
kde-la-c <kdelacolinaplant@gmail.com>
2024-12-08 00:45:49 +0100

    day7 failure

--- Day07/main.cpp
+++ Day07/main.cpp
@@ -1,24 +1,40 @@
 #include "../AoC.hpp"
 
+int	result_checker(vector<long> equation, int pos)
+{
+	return (0);
+}
+
 int main()
 {
-	ifstream			input("input.txt");
-	string				line;
-	vector<vector<int>>	equations;
-	int					ret = 0;
+	ifstream				input("input.txt");
+	string					line;
+	vector<vector<long>>	equations;
+	int						ret = 0;
 
 	for (int i = 0; getline(input, line); i++)
 	{
 		istringstream	str(line);	
-		vector<int>		equation = equations.at(i);
+		vector<long>		equation;
 		do
 		{
 			string lvl;
 			str >> lvl;
 			equation.push_back(atoi(lvl.c_str()));
-		} while (str);		
+		} while (str);
+		equations.push_back(equation);
 	}
 
+	for (int i = 0; i < equations.size(); i++)
+	{
+		for (int j = 0; j < equations.at(i).size(); j++)
+		{
+			if (result_checker(equations.at(i), 1))
+				ret += equations.at(i).at(0);
+		}
+		
+	}
+	
 
 	cout << ret << endl;
 }

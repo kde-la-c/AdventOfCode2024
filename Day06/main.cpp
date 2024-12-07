2d9ab7f0e345de3b8411869b9222bc792e859f02
kde-la-c <kdelacolinaplant@gmail.com>
2024-12-07 04:05:38 +0100

    failed day6.2, commented implementation idea

--- Day06/main.cpp
+++ Day06/main.cpp
@@ -1,5 +1,14 @@
 #include "../AoC.hpp"
 
+/* 
+didn't manage to implement algorithm correctly due to a bad structure to begin
+with.
+It would have been a check each time we are on a 'x' cell if the line to it's
+right would end with 'X' then '#', and be full of 'x' cells.
+This would have ensured a possible infinite loop in case we placed an obstacle
+just in front of the guard's current position.
+ */
+
 typedef enum e_direction
 {
 	UP,
@@ -35,37 +44,60 @@ coords::~coords()
 }
 coords cords(0, 0);
 
-bool	check_ahead(vector<string> map, int dir)
+bool	check_ahead(vector<string> map, int dir, coords crd)
 {
-	coords	tmp(cords);
+	coords	tmp(crd.l, crd.c);
+	bool ret = false;
 	if (dir == UP)
 	{
 		tmp.l--;
-		return (tmp.isOnRange() && map.at(cords.l - 1).at(cords.c) != '#');
+		ret = (tmp.isOnRange() && map.at(cords.l - 1).at(cords.c) != '#');
 	}
 	else if (dir == RIGHT)
 	{
 		tmp.c++;
-		return (tmp.isOnRange() && map.at(cords.l).at(cords.c + 1) != '#');
+		ret = (tmp.isOnRange() && map.at(cords.l).at(cords.c + 1) != '#');
 	}
 	else if (dir == DOWN)
 	{
 		tmp.l++;
-		return (tmp.isOnRange() && map.at(cords.l + 1).at(cords.c) != '#');
+		ret = (tmp.isOnRange() && map.at(cords.l + 1).at(cords.c) != '#');
 	}
 	else if (dir == LEFT)
 	{
 		tmp.c--;
-		return (tmp.isOnRange() && map.at(cords.l).at(cords.c - 1) != '#');
+		ret = (tmp.isOnRange() && map.at(cords.l).at(cords.c - 1) != '#');
 	}
-	return (false);
+	// cout << "checked " << tmp.l << ' ' << tmp.c << endl;
+	return (ret);
+}
+
+bool	check_loop(vector<string> *map, int dir)
+{
+	coords tmp(cords.l, cords.c);
+	dir = (dir + 1) % 4;
+	while (tmp.isOnRange() && check_ahead(*map, dir, tmp))
+	{
+		cout << '	' << map->at(tmp.l).at(tmp.c) << endl;
+		if (dir == UP)
+			tmp.l--;
+		else if (dir == RIGHT)
+			tmp.c++;
+		else if (dir == DOWN)
+			tmp.l++;
+		else if (dir == LEFT)
+			tmp.c--;
+	}
+	// cout << map->at(tmp.l).at(tmp.c) << " " << tmp.l << tmp.c << endl;
+	// cout << (map->at(tmp.l).at(tmp.c) == 'X') << endl;
+	return (map->at(tmp.l).at(tmp.c) == 'X');
 }
 
 int	go(vector<string> *map, int direction)
 {
 	int	ret = 0, dir = direction % 4;
 
-	while (cords.isOnRange() && check_ahead(*map, dir))
+	while (cords.isOnRange() && check_ahead(*map, dir, cords))
 	{
 		if (map->at(cords.l).at(cords.c) == '.')
 		{
@@ -80,12 +112,16 @@ int	go(vector<string> *map, int direction)
 			cords.l++;
 		else if (dir == LEFT)
 			cords.c--;
+		if (check_loop(map, dir))
+			ret++;
 	}
 	if (cords.isOnRange() && map->at(cords.l).at(cords.c) == '.')
 	{
 		ret++;
-		map->at(cords.l).at(cords.c) = 'x';
+		map->at(cords.l).at(cords.c) = 'X';
 	}
+	// if ()
+	// 	return (-1);
 	return (ret);
 }
 
@@ -95,7 +131,7 @@ int main()
 	ofstream		output("out.txt");
 	string			line;
 	vector<string>	map;
-	int				ret = 0, tmp;
+	int				ret1 = 0, ret2 = 0, tmp;
 
 	for (; getline(input, line); )
 	{
@@ -113,17 +149,29 @@ int main()
 				break;
 			}
 	
+	// for (int i = 0; cords.isOnRange(); i++)
+	// {
+	// 	tmp = go(&map, i);
+	// 	if (tmp <= 0)
+	// 		break;
+	// 	ret1 += tmp;
+	// }
+	// cout << ret1 + 1 << endl;
 	for (int i = 0; cords.isOnRange(); i++)
 	{
+		this_thread::sleep_for (std::chrono::milliseconds(100));
+
+		cout << "hola" << endl;
 		tmp = go(&map, i);
 		if (tmp <= 0)
 			break;
-		ret += tmp;
+		ret2 += tmp;
 	}
+	cout << ret2 << endl;
 	
 	for (int l = 0; l < height; l++)
 	{
 		output << map.at(l) << endl;
 	}
-	cout << ret + 1 << endl;
 }
+
